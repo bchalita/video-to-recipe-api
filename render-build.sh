b9edbec319920a2ab8a3bf040e026867633c5287
bchalita <100321607+bchalita@users.noreply.github.com>
2025-04-09 21:21:24 -0400

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -5,5 +5,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "[BUILD] Checking NumPy installation..."
-pip show numpy
 python -c "import numpy; print('[RENDER] NumPy is available:', numpy.__version__)"
