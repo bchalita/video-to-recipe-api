2e2bd647c3ac252ad2a6b7599bb46846584b45cd
bchalita <100321607+bchalita@users.noreply.github.com>
2025-04-30 11:36:26 -0400

    Delete init_db.py

--- init_db.py
+++
@@ -1,5 +0,0 @@
-from scrape_ingredients import init_ingredients_db, ingredient_db_path
-
-if __name__ == "__main__":
-    init_ingredients_db(ingredient_db_path)
-    print("✅ ingredients.db initialized or already exists.")
